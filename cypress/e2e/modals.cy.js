faa3b10a818330dee3cb40d174b2a2f2da9501b1
pavithra subramanian <pavithra.subramanian@gmail.com>
2024-09-03 13:06:13 +0530

    modals added

---
+++ cypress/e2e/modals.cy.js
@@ -0,0 +1,21 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('https://practice-automation.com/modals/')
+  })
+
+  it('Assert simple modal', () => {
+    cy.get('#simpleModal').should('be.visible').click()
+    cy.wait(500)
+    cy.get('p').contains('Hi, I’m a simple modal.').should('be.visible')
+    cy.get('#popmake-1318').should('be.visible')
+    cy.get('[aria-label="Close"]').eq(1).click()
+  })
+  it('Assert form modal', () => {
+    cy.get('#formModal').should('be.visible').click()
+    cy.wait(500)
+    cy.get('#pum_popup_title_674').contains('Modal Containing A Form').should('be.visible')
+    cy.get('#g1051-name').should('be.visible').should('be.visible').type('Pavi')
+    cy.get('#g1051-email').should('be.visible').should('be.visible').type('Pavi@abc.com')
+    cy.get('.pushbutton-wide').should('be.visible').click()
+  })
+})
